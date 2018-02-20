a15a08c266028fd177dc8c3fc8c2e1e9b8434f0a
Rui Silva <Rui.Silva@devscope.net>
2018-02-20 12:54:24 +0000

    Added collapse function to webchat

--- src/botFramework.js
+++ src/botFramework.js
@@ -1,81 +1,122 @@
-﻿(function () {
+﻿(function() {
     'use strict';
 
-	var constants = {
-		tokenUrl:"https://webchat.botframework.com/api/tokens",
-		iframeUrl:"https://webchat.botframework.com/embed/"
-	};
+    var constants = {
+        tokenUrl: "https://webchat.botframework.com/api/tokens",
+        iframeUrl: "https://webchat.botframework.com/embed/"
+    };
 
-	var defaultConfig = {
-		bot_id:null,
-		secret: null,
-		width: "300px",
-		height: "450px",
-		bottom: "0",
-		top: null,
-		position: "sticky",
-		border: "1px solid #DBDEE1",
-		left: "null",
-		left: "calc(100% - 300px)"
-	};
+    var defaultConfig = {
+        bot_id: null,
+        secret: null,
+        width: "300px",
+        height: "450px",
+        bottom: "0",
+        top: null,
+        position: "sticky",
+        background: "white",
+        border: "1px solid #DBDEE1",
+        left: "null",
+        left: "calc(100% - 300px)"
+    };
 
-	var config = defaultConfig;
+    var config = defaultConfig;
 
     var botIFrame = {
-    	config: function (botConfig) {
-			if (botConfig.bot_id == null || botConfig.bot_id == undefined || botConfig.bot_id == "") {
-				return "bot_id is required!";
-			}
+        config: function(botConfig) {
+            if (botConfig.bot_id == null || botConfig.bot_id == undefined || botConfig.bot_id == "") {
+                return "bot_id is required!";
+            }
 
-			if (botConfig.secret == null || botConfig.secret == undefined || botConfig.secret == "") {
-				return "secret is required!";
-			}
+            if (botConfig.secret == null || botConfig.secret == undefined || botConfig.secret == "") {
+                return "secret is required!";
+            }
 
-    		config.bot_id = botConfig.bot_id;
-    		config.secret = botConfig.secret;
+            config.bot_id = botConfig.bot_id;
+            config.secret = botConfig.secret;
 
-			config.width = botConfig.width || config.width;
-			config.height = botConfig.height || config.height;
-			config.bottom = botConfig.bottom || config.bottom;
-			config.top = botConfig.top || config.top;
-			config.position = botConfig.position || config.position;
-			config.border = botConfig.border || config.border;
-			config.left = botConfig.left || config.left;
-			config.right = botConfig.right || config.right;
+            config.width = botConfig.width || config.width;
+            config.height = botConfig.height || config.height;
+            config.bottom = botConfig.bottom || config.bottom;
+            config.top = botConfig.top || config.top;
+            config.background = botConfig.background || config.background;
+            config.position = botConfig.position || config.position;
+            config.border = botConfig.border || config.border;
+            config.left = botConfig.left || config.left;
+            config.right = botConfig.right || config.right;
 
-			var callback = function (result) {
-				var iframe = document.querySelector('#botFramework');
-				if (iframe == null || iframe == undefined) {
-					iframe = document.createElement("iframe");
-				}
+            var callback = function(result) {
+                var iframeContainer = document.querySelector('#botFrameworkContainer');
+                if (iframeContainer == null || iframeContainer == undefined) {
+                    iframeContainer = document.createElement("div");
+                }else{
+	                iframeContainer.innerHTML = "";
+                }
 
-				try{
-					iframe.setAttribute("id","botFramework");
-					iframe.setAttribute("src", constants.iframeUrl + config.bot_id + "?t=" + result.slice(1, -1));
-					iframe.style.width = config.width;
-					iframe.style.height = config.height;
-					iframe.style.bottom = config.bottom;
-					iframe.style.top = config.top;
-					iframe.style.position = config.position;
-					iframe.style.border = config.border;
-					iframe.style.left = config.left;
-					iframe.style.right = config.right;
-					document.body.appendChild(iframe);
-				}catch(e){
-					console.log(e);
-				}
-			};
+                var iframe = document.querySelector('#botFramework');
+                if (iframe == null || iframe == undefined) {
+                    iframe = document.createElement("iframe");
+                }
 
-    		var xmlHttp = new XMLHttpRequest();
-		    xmlHttp.onreadystatechange = function() { 
-		        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-		            callback(xmlHttp.responseText);
-		    }
-		    xmlHttp.open("GET", constants.tokenUrl, true); // true for asynchronous 
-		    xmlHttp.setRequestHeader("Authorization", "BotConnector " + config.secret);
-		    xmlHttp.send(null);
-    	}
+                try {
+                    iframeContainer.setAttribute("id", "botFrameworkContainer");
+                    iframeContainer.style.width = config.width;
+                    iframeContainer.style.height = config.height;
+                    iframeContainer.style.bottom = config.bottom;
+                    iframeContainer.style.top = config.top;
+                    iframeContainer.style.background = config.background;
+                    iframeContainer.style.position = config.position;
+                    iframeContainer.style.border = config.border;
+                    iframeContainer.style.left = config.left;
+                    iframeContainer.style.right = config.right;
+
+                    var containerHeader = document.createElement("div");
+                    containerHeader.style.float = "right";
+                    containerHeader.style["margin-bottom"] = "-36px";
+                    containerHeader.style.color = "white";
+                    containerHeader.style["margin-top"] = "5px";
+                    containerHeader.style["margin-right"] = "10px";
+                    containerHeader.style.cursor = "pointer";
+
+                    var icon = document.createElement("i");
+                    icon.setAttribute("id","bot-header-collapse");
+                    icon.className = "fa fa-angle-down fa-2x";
+                    containerHeader.appendChild(icon);
+
+                    containerHeader.onclick = function() {
+                        if (iframeContainer.style.height == config.height) {
+                    		icon.className = "fa fa-angle-up fa-2x";
+                            iframeContainer.style.height = '40px';
+                        } else {
+                    		icon.className = "fa fa-angle-down fa-2x";
+                            iframeContainer.style.height = config.height;
+                        }
+                    }
+                    iframeContainer.appendChild(containerHeader);
+
+                    iframe.setAttribute("id", "botFramework");
+                    iframe.setAttribute("src", constants.iframeUrl + config.bot_id + "?t=" + result.slice(1, -1));
+                    iframe.style.height = '100%';
+                    iframe.style.border = '0';
+
+                    iframeContainer.appendChild(iframe);
+                    document.body.appendChild(iframeContainer);
+                } catch (e) {
+                    console.log(e);
+                }
+            };
+
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function() {
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                    callback(xmlHttp.responseText);
+            }
+            xmlHttp.open("GET", constants.tokenUrl, true); // true for asynchronous 
+            xmlHttp.setRequestHeader("Authorization", "BotConnector " + config.secret);
+            xmlHttp.send(null);
+        }
     };
+
 
     window.botFramework = botIFrame;
 })();
